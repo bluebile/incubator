4211897c9c60c0c17e933bfd083418ce638c0726
viniciusgandrade <viniciusgandrade@gmail.com>
2016-08-25 21:48:35 -0300

    inclusão de método para tratamento de erros de ajax

--- service/Service.js
+++ service/Service.js
@@ -17,5 +17,28 @@ Ext.define('Mba.ux.incubator.service.Service', {
         this.initConfig(config);
     },
 
+    tratarErro: function(response, deferred) {
+        var mensagem, textResponse;
+        if (response.responseText === '') {
+            deferred.reject('O serviço está indisponível no momento.');
+            return;
+        }
+
+        textResponse = Ext.decode(response.responseText);
+        if (response.status == 405) {
+            deferred.reject(textResponse.detail);
+        } else if (response.status == 400 || response.status == 401) {
+            mensagem = textResponse.detail;
+            if (mensagem == 'Dados incorretos') {
+                mensagem = 'MSG_ERRO_LOGIN';
+            }
+            deferred.reject(mensagem);
+        } else if (response.status == 200) {
+            deferred.reject('MSG_ERRO_AUTENTICAR');
+        } else {
+            deferred.reject('MSG_ERRO_TIMEOUT');
+        }
+    },
+
     init: Ext.emptyFn
 });
