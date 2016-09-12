0ef960a894cc3fbb0e92d759559b1503b65275b8
viniciusgandrade <viniciusgandrade@gmail.com>
2016-09-12 10:17:06 -0300

    Update Toast.js

--- util/Toast.js
+++ util/Toast.js
@@ -1,12 +1,13 @@
 Ext.define('Mba.ux.incubator.util.Toast', {
     extend: 'Ext.Toast',
+    singleton: true,
+    alternateClassName: 'MbaToast',
     config: {
         cls: 'error-message'
     },
 
     show: function(message) {
-        var config = { timeout: 8000, message: message};
+        var config = { timeout: 2000, message: message};
         this.callParent([config]);
     }
 });
-
